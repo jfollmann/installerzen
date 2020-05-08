80e3e7395951094b2adeb77bbd1f5d28f0cd5c49
Jefferson Follmann <jeff.follmann@gmail.com>
2020-05-07 22:56:26 -0300

    Increment verion && Task to copy README.md

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,14 @@ gulp.task('clean', function () {
   return del('./dist/src/apps/sh/**', { force: true });
 })
 
+gulp.task('readme', function () {
+  return gulp.src('./README.md')
+    .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('shellScriptFiles', function () {
   return gulp.src('./src/apps/sh/*.sh')
     .pipe(gulp.dest('./dist/src/apps/sh'));
 });
 
-gulp.task('default', gulp.parallel('clean', 'shellScriptFiles'));
+gulp.task('default', gulp.parallel('clean', 'readme', 'shellScriptFiles'));
